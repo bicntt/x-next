ef492a3d00aaa9b94a427f33a0afaa36ca511ad1
bicntt <bicntt@gmail.com>
2024-05-15 15:36:00 +0700

    Modify the sidebar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,14 +12,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex justify-between max-w-6xl mx-auto">
-            <div><Sidebar /></div>
-            <div>{children}</div>
-            <div><News /></div>
-        </div>
-      </body>
-    </html>
-  );
+      <html lang="en">
+          <body className={inter.className}>
+              <div className="flex justify-between max-w-6xl mx-auto">
+                  <div className="hidden sm:inline border-r h-screen">
+                      <Sidebar />
+                  </div>
+                  <div>{children}</div>
+                  <div>
+                      <News />
+                  </div>
+              </div>
+          </body>
+      </html>
+  )
 }
