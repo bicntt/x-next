ec4a2965bac6df7ff702794e54fb0a05dc1e3dd1
bicntt <bicntt@gmail.com>
2024-05-30 16:02:57 +0700

    add firebase storage and complete file upload functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,62 @@
 'use client'
 import { useSession } from 'next-auth/react'
+import { useEffect, useRef, useState } from 'react'
 import { HiOutlinePhotograph } from 'react-icons/hi'
+import {app} from '../firebase'
+import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
 
 export default function Input() {
     const { data: session } = useSession()
+    const [imageFileUrl, setImageUrl] = useState(null)
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [imageFileUploading, setImageFileUploading] = useState(false)
+
+    const imagePickRef = useRef(null)
+    console.log(imageFileUrl)
+
+
+    const addImageToPost = (e) => {
+        const file = e.target.files[0]
+
+        if (file) {
+            setSelectedFile(file)
+            setImageUrl(URL.createObjectURL(file))
+        }
+    }
+
+    useEffect(()=>{
+        if(selectedFile) {
+            uploadImageToStorage()
+        }
+    },[selectedFile])
+
+    const uploadImageToStorage = () => {
+        setImageFileUploading(true)
+        const storage = getStorage(app)
+        const fileName = new Date().getTime() + '-' + selectedFile.name
+        const storageRef = ref(storage, fileName)
+        const uploadTask = uploadBytesResumable(storageRef, selectedFile)
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                console.log('Upload is ' + progress + '% done')
+            },
+            (error) => {
+                console.log(error) 
+                setImageFileUploading(false)
+                setImageUrl(null)
+                setSelectedFile(null)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                    setImageUrl(downloadURL)
+                    setImageFileUploading(false)
+                })
+            }
+        )
+
+    }
 
     if (!session) return null
     return (
@@ -20,8 +73,26 @@ export default function Input() {
                     placeholder="Whats happening"
                     rows="2"
                 ></textarea>
+
+                {
+                    selectedFile && (
+                        <img src={imageFileUrl} alt="image" className='w-full max-h-[250px] object-cover cursor-pointer' />
+
+                    )
+                }
+
                 <div className=" flex items-center justify-between pt-2.5">
-                    <HiOutlinePhotograph className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
+                    <HiOutlinePhotograph
+                        onClick={() => imagePickRef.current.click()}
+                        className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer"
+                    />
+                    <input
+                        type="file"
+                        ref={imagePickRef}
+                        accept="image/*"
+                        onChange={addImageToPost}
+                        hidden
+                    />
                     <button
                         disabled
                         className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: 'x-next-424108.firebaseapp.com',
+    projectId: 'x-next-424108',
+    storageBucket: 'x-next-424108.appspot.com',
+    messagingSenderId: '211617359109',
+    appId: '1:211617359109:web:6b09c1c08b783ad647036c',
+    measurementId: 'G-0E1EEEBK25',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
